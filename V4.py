9a89c7bee881a7f000b2b323580eb60353699906
piair <piair338@gmail.com>
2022-03-28 18:33:14 +0200

    oops

--- V4.py
+++ V4.py
@@ -89,8 +89,9 @@ def FirefoxPC(Headless = Headless):
     PC_USER_AGENT = ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                 'AppleWebKit/537.36 (KHTML, like Gecko) '
                 'Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134')
-    options.set_preference("general.useragent.override", PC_USER_AGENT)
     options = Options()
+    options.set_preference("general.useragent.override", PC_USER_AGENT)
+    
     options.set_preference("browser.link.open_newwindow", 3)
     
     if Headless :
