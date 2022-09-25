b6e30888bb9b108b582867a08c0322c316759bd1
augustin64 <67148092+augustin64@users.noreply.github.com>
2022-09-25 15:32:34 +0200

    Update V4.py
    Fix constant name (introduced in [this commit](https://github.com/piair338/MsReward/commit/5b3b7c8aecdc9f0cf3d3ddfd08416356a6ecc544))

--- V4.py
+++ V4.py
@@ -223,7 +223,7 @@ def FirefoxDriver(mobile=False, Headless=Headless):
 
 
 def printf(txt, end="", Mobdriver=driver):
-    if Log:
+    if LOG:
         print(Timer(txt))
     if FULL_LOG:
         try :
@@ -234,7 +234,7 @@ def printf(txt, end="", Mobdriver=driver):
 
 def CustomSleep(temps):
     try : 
-        if Log or not LINUX_HOST: #only print sleep when user see it
+        if LOG or not LINUX_HOST: #only print sleep when user see it
             points = [
                 " .   ",
                 "  .  ",
